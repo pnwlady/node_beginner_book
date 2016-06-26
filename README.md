9c70dd5a265c737341c75b3e2f897b24e6fd0c77
heidilaursen <heidimlaursen@gmail.com>
2016-06-26 16:54:20 -0700

    server updated

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # node_beginner_book
 
 401 Week one assignment.
+
+hellowWworld.js consolelogs a message
+index.js routes and handles requests
+router.js exports a route function to handling routing
+server.js exports a start function to start a server
--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const url = require('url');
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log('Request for ' + pathname + ' recieved.');
+    console.log("request for " + pathname + " recieved");
+    route(handle, pathname, response, request);
+  }
+}
 
-    route(handle, pathname);
-
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello World');
-    response.end();
-  };
-  http.createServer(onRequest).listen(8888);
-  console.log();
-};
+http.createServer(onRequest).listen(8888);
+console.log('server up on port 8888');
 
 exports.start = start;
