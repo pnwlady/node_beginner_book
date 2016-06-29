bcade9c1f58a9be65946b8c48e4d8dcce1fba0c5
heidilaursen <heidimlaursen@gmail.com>
2016-06-29 09:11:16 -0700

    adding body html to request handler start()

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,19 @@ var exec = require('child_process').exec;
 
 function start(response) {
   console.log("Request handler 'start' was called");
-  var content = "empty";
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
 
   exec('ls -lah', function (error, stdout, stderr) {
     response.writeHead(200, {"Content-Type": "text/plain"});
