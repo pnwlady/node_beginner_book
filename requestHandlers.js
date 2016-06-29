50d5c7693efc27247d397af550a672eb79dccb0f
heidilaursen <heidimlaursen@gmail.com>
2016-06-29 12:39:50 -0700

    adding tmp dir and test.png file, added show handler and route to index

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,7 @@
-var exec = require('child_process').exec;
-var querystring = require('querystring');
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
+
 
 function start(response, postData) {
   console.log("Request handler 'start' was called");
@@ -10,15 +12,14 @@ function start(response, postData) {
     'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" '+
-    'method="post">'+
-    '<input type="file" name="upload" multiple="multiple">'+
-    '<input type="submit" value="Upload file" />'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
     '</form>'+
     '</body>'+
     '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
 }
@@ -30,5 +31,12 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream('/tmp/test.png').pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
