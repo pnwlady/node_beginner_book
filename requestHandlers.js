16e50fac1fe049eb5c501604c0cb586a19c55bdb
heidilaursen <heidimlaursen@gmail.com>
2016-06-29 12:57:13 -0700

    updated upload function for images

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,7 @@ var querystring = require("querystring"),
     formidable = require("formidable");
 
 
-function start(response, postData) {
+function start(response) {
   console.log("Request handler 'start' was called");
 
   var body = '<html>'+
@@ -12,7 +12,7 @@ function start(response, postData) {
     'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" '+'method="post">'+'<input type="file" name="upload">'+
+    '<form action="/upload" enctype="multipart/form-data" '+'method="post">'+'<input type="file" name="upload" multiple="multiple">'+
     '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
@@ -23,32 +23,27 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
-  console.log("Request handler 'upload' was called");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: "+ querystring.parse(postData).text);
-  response.end();
-}
-
 function upload(response, request) {
-  console.log("Request handler 'upload' was called.");
+  console.log("Request handler 'upload' was called");
 
   var form = new formidable.IncomingForm();
   console.log("about to parse");
   form.parse(request, function(error, fields, files) {
     console.log("parsing done");
 
-    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
-      if (err) {
+    fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+      if (error) {
         fs.unlink("/tmp/test.png");
         fs.rename(files.upload.path, "/tmp/test.png");
       }
     });
+
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("received image:<br/>");
+    response.write("recieved image:<br/> ");
     response.write("<img src='/show' />");
     response.end();
   });
+
 }
 
 function show(response) {
